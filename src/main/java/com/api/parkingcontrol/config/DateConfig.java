05c3e49bfa2b365643490529ed61f84f5ba935bf
unknown <lfernandes.java@gmail.com>
2022-03-01 14:54:52 -0300

    Teste Branch Novo

--- src/main/java/com/api/parkingcontrol/config/DateConfig.java
+++ src/main/java/com/api/parkingcontrol/config/DateConfig.java
@@ -14,7 +14,7 @@ public class DateConfig {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     public static LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER = new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT));
-
+/*teste branch novo*/
     @Bean
     @Primary
     public ObjectMapper objectMapper(){
